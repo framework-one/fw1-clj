dcfde6d76213991b494663abd789a017fea5818d
Sean Corfield <sean@worldsingles.com>
2011-11-04 23:10:47 -0700

    Update to Clojure 1.3.0 and latest Ring.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject fw1-clj "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [ring "0.3.8"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring "1.0.0-RC1"]
                  [enlive "1.0.0"]])
