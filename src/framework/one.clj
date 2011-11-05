d286cd817c88ca2875872e21eba211546fb95c6c
Sean Corfield <sean@corfield.org>
2011-11-05 12:00:32 -0700

    Add before / after. Add full route / query string handling to RC. Start work on layouts.

--- src/framework/one.clj
+++ src/framework/one.clj
@@ -1,4 +1,5 @@
 (ns framework.one
+  (:require [clojure.walk :as walk])
   (:require [ring.middleware.params :as ring-p])
   (:require [net.cgrand.enlive-html :as html]))
 
@@ -15,31 +16,51 @@
 ;; FW/1 base functionality
 
 (declare config)
-(declare view-cache)
+(def ^:private node-cache (atom {}))
 
-(defn- parts [req] (rest (clojure.string/split (:uri req) #"/")))
+(defn- parts [req] (rest (.split (:uri req) "/")))
 
-(defn- view [section item]
-  (or (get @view-cache [section item])
-      (let [view-nodes (html/html-resource (str "views/" section "/" item ".html"))]
-        (swap! view-cache #(assoc % [section item] view-nodes))
-        view-nodes)))
+(defn- get-cached-nodes [node-key node-path & {:keys [required] :or {required false}}]
+  (or (get @node-cache node-key)
+      (let [nodes (try
+                    (html/html-resource node-path)
+                    (catch Exception e
+                      (if required (throw e) nil)))]
+        (swap! node-cache #(assoc % node-key nodes))
+        nodes)))
+
+(defn- get-view-nodes [section item]
+  (get-cached-nodes [:view section item] (str "views/" section "/" item ".html") :required true))
+
+(defn- get-layout-nodes [section item]
+  [(get-cached-nodes [:layout section item] (str "layouts/" section "/" item ".html"))
+   (get-cached-nodes [:layout section] (str "layouts/" section ".html"))
+   (get-cached-nodes [:layout] "layouts/default.html")])
+
+(defn- as-map [route]
+  (apply hash-map
+         (if (even? (count route))
+           route
+           (concat route [""]))))
+
+(defn- apply-controller [controller-ns rc item]
+  (if-let [f (resolve (symbol (str controller-ns "/" item)))] (f rc) rc))
 
 (defn- controller [req]
   (let [config @config
         route (parts req)
-        rc (:params req)
+        rc (walk/keywordize-keys (merge (as-map (rest (rest route))) (:params req)))
         section (or (first route) (:default-section config))
         item (or (second route) (:default-item config))
         controller-ns (symbol (str "controllers." section))
         _ (if (:reload-application-on-every-request config)
-            (require controller-ns :reload)
+            (do
+              (reset! node-cache {})
+              (require controller-ns :reload))
             (require controller-ns))
-        target (str controller-ns "/" item)
-        controller (resolve (symbol target))
-        rc (if controller (controller rc) rc)
-        view-process (resolve (symbol (str target "-view")))
-        view-nodes (view section item)
+        rc (reduce (partial apply-controller controller-ns) rc ["before" item "after"])
+        view-process (resolve (symbol (str controller-ns "/" item "-view")))
+        view-nodes (get-view-nodes section item)
         view-render (if view-process
                       (view-process view-nodes rc)
                       view-nodes)
@@ -55,12 +76,16 @@
      :body "favicon.ico not found"}
     ((ring-p/wrap-params controller) req)))
 
+(defn- framework-defaults [options]
+  (if (:error options)
+    (assoc options :error (.split (:error options) "."))
+    (assoc options :error [(:default-section options) "error"])))
+
 (defn start [& app-config]
   (def ^:private config (atom {}))
-  (def ^:private view-cache (atom {}))
   (let [defaults {:default-section "main"
                   :default-item "default"
                   :reload-application-on-every-request false}
-        my-config (merge defaults (apply hash-map app-config))]
+        my-config (framework-defaults (merge defaults (apply hash-map app-config)))]
     (reset! config my-config)
     (var wrapper)))
