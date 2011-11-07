e29df382330d0091b985ac8c59c2afe59bd9af4d
Sean Corfield <sean@corfield.org>
2011-11-06 16:45:44 -0800

    Add some mock data. Implement form display for add/edit.

--- src/example/controllers/user.clj
+++ src/example/controllers/user.clj
@@ -1,13 +1,39 @@
 (ns example.controllers.user
   (:use framework.one))
 
+;; mock data
+(def ^:private departments
+  [{:id 1 :name "Accounting"}
+   {:id 2 :name "Sales"}
+   {:id 3 :name "Support"}
+   {:id 4 :name "Development"}])
+
+(def ^:private initial-user-data
+  [{:id 1 :first-name "Sean" :last-name "Corfield" :email "sean@worldsingles.com" :department-id 3}])
+
+(def ^:private users (atom initial-user-data))
+
+;; data access methods - would be in a service
+(defn- get-user-by-id [id]
+  (first (filter #(== id (:id %)) @users)))
+
+(defn- to-long [l]
+  (try (Long/parseLong l) (catch Exception _ 0)))
+
 ;; controller methods
 (defn default [rc]
-  (assoc rc :message "main.default controller called"))
+  (assoc rc :message "Welcome to the Framework One User Manager application demo!"))
 
-(defn form [rc])
+(defn form [rc]
+  ;; fetch user based on id
+  (assoc rc :user (get-user-by-id (to-long (:id rc)))))
 
-(defn list [rc])
+(defn list [rc]
+  rc)
+
+(defn save [rc]
+  ;; need redirect!
+  )
 
 ;; view methods
 (defn default-view [rc nodes]
@@ -17,6 +43,21 @@
                     (content "The framework cache (and application scope) have been reset.")
                     (substitute ""))))
 
-(defn form-view [rc nodes])
+(defn form-view [rc nodes]
+  (let [user (:user rc)]
+    (at nodes
+        [:input#id] (set-attr :value (or (:id user) 0))
+        [:input#first-name] (set-attr :value (:first-name user))
+        [:input#last-name] (set-attr :value (:last-name user))
+        [:input#email] (set-attr :value (:email user))
+        [:select#department-id :option]
+        (clone-for [dept departments]
+                   (do->
+                     (set-attr :value (:id dept))
+                     (if (= (:department-id user) (:id dept))
+                       (set-attr :selected "selected")
+                       identity)
+                     (content (:name dept)))))))
 
-(defn form-list [rc nodes])
+(defn form-list [rc nodes]
+  nodes)
