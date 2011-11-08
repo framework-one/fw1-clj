c97f13e94e398a7004e469807d66128da9d98a58
Sean Corfield <sean@corfield.org>
2011-11-07 16:14:07 -0800

    Split data access to a separate namespace. Completed delete, save and tidied up form and list.

--- src/example/controllers/user.clj
+++ src/example/controllers/user.clj
@@ -1,52 +1,33 @@
 (ns example.controllers.user
-  (:use framework.one))
-
-;; mock data
-(def ^:private departments
-  [{:id 1 :name "Accounting"}
-   {:id 2 :name "Sales"}
-   {:id 3 :name "Support"}
-   {:id 4 :name "Development"}])
-
-(def ^:private initial-user-data
-  [{:id 1 :first-name "Sean" :last-name "Corfield" :email "sean@worldsingles.com" :department-id 3}])
-
-(def ^:private users (atom initial-user-data))
-
-;; data access methods - would be in a service
-(defn- get-department-by-id [id]
-  (first (filter #(== id (:id %)) departments)))
-
-(defn- get-user-by-id [id]
-  (first (filter #(== id (:id %)) @users)))
-
-(defn- to-long [l]
-  (try (Long/parseLong l) (catch Exception _ 0)))
+  (:use framework.one)
+  (:use example.model.user-manager))
 
 ;; controller methods
 (defn default [rc]
   (assoc rc :message "Welcome to the Framework One User Manager application demo!"))
 
 (defn delete [rc]
-  ;; need redirect!
+  (delete-user-by-id (to-long (:id rc)))
   (redirect rc "/user/list"))
 
 (defn form [rc]
-  ;; fetch user based on id
-  (assoc rc :user (get-user-by-id (to-long (:id rc)))))
+  (let [user (get-user-by-id (to-long (:id rc)))]
+    (assoc rc :user user)))
 
 (defn list [rc]
-  (assoc rc :users @users))
+  (let [users (get-users)]
+    (assoc rc :users users)))
 
 (defn save [rc]
-  ;; need redirect!
+  (let [{:keys [id first-name last-name email department-id]} rc]
+    (save-user {:id (to-long id) :first-name first-name :last-name last-name :email email :department-id (to-long department-id)}))
   (redirect rc "/user/list"))
 
 ;; view methods
 (defn default-view [rc nodes]
   (at nodes
       [:p#message] (content (:message rc))
-      [:p#reload] (if (:reload rc)
+      [:p#reload] (if (reload? rc)
                     (content "The framework cache (and application scope) have been reset.")
                     (substitute ""))))
 
@@ -58,7 +39,7 @@
         [:input#last-name] (set-attr :value (:last-name user))
         [:input#email] (set-attr :value (:email user))
         [:select#department-id :option]
-        (clone-for [dept departments]
+        (clone-for [dept (get-departments)]
                    (do->
                      (set-attr :value (:id dept))
                      (if (= (:department-id user) (:id dept))
---
+++ src/example/model/user_manager.clj
@@ -0,0 +1,41 @@
+(ns example.model.user-manager)
+
+;; mock data
+(def ^:private departments
+  [{:id 1 :name "Accounting"}
+   {:id 2 :name "Sales"}
+   {:id 3 :name "Support"}
+   {:id 4 :name "Development"}])
+
+(def ^:private initial-user-data
+  [{:id 1 :first-name "Sean" :last-name "Corfield" :email "sean@worldsingles.com" :department-id 4}])
+
+(def ^:private users (atom {}))
+
+(reset! users initial-user-data)
+
+(defn- new-id [rows]
+  (if (empty? rows) 1 (inc (:id (apply max-key :id rows)))))
+
+(defn get-department-by-id [id]
+  (first (filter #(= id (:id %)) departments)))
+
+(defn get-departments []
+  departments)
+
+(defn get-user-by-id [id]
+  (first (filter #(= id (:id %)) @users)))
+
+(defn get-users []
+  (sort-by :id @users))
+
+(defn save-user [user]
+  (let [id (:id user)]
+    (if (zero? id)
+      ;; insert
+      (swap! users #(conj % (assoc user :id (new-id %))))
+      ;; update
+      (swap! users #(map (fn [row] (if (= id (:id row)) user row)) %)))))
+
+(defn delete-user-by-id [id]
+  (swap! users (partial remove #(= id (:id %)))))
