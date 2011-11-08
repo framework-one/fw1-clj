1507d93799dfd1408d26fdff6eec84e4974b2b07
Sean Corfield <sean@worldsingles.com>
2011-11-07 20:29:51 -0800

    Bump to 0.0.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject framework-one "0.0.2-SNAPSHOT"
+(defproject framework-one "0.0.2"
   :description "A lightweight, convention-based MVC web framework."
   :dev-resources-path "examples"
   :dependencies [[org.clojure/clojure "1.3.0"]
