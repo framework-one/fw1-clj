19eeffb768c5e2673b5a63207b48ce8ce1ee708d
Sean Corfield <sean@worldsingles.com>
2012-04-17 23:06:14 -0700

    Bump to 0.1.0; Update to Clojure 1.4.0 & Ring 1.0.2.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject framework-one "0.0.6"
+(defproject framework-one "0.1.0"
   :description "A lightweight, convention-based MVC web framework."
   :dev-resources-path "examples"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring "1.0.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring "1.0.2"]
                  [enlive "1.0.0"]])
