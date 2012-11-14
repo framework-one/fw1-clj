9b97f57982c1036e5d4be7b52073b0db0cb2e94a
Sean Corfield <sean@worldsingles.com>
2012-11-14 15:01:44 -0800

    Add lein2 compatibility.
    Ignore target/ folder.
    Add lein2 style resource-paths (as opposed to lein1 resources-path!).

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject framework-one "0.1.0"
   :description "A lightweight, convention-based MVC web framework."
   :dev-resources-path "examples"
+  :profiles {:dev {:resource-paths ["examples"]
+                   :dependencies []}}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.0.2"]
                  [enlive "1.0.0"]])
