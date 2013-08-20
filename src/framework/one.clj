467f6fcc513c51175bae0f8662a16c51eb0a5a0d
Sean Corfield <sean@corfield.org>
2013-08-19 18:39:34 -0700

    Prep for 0.2.1 release

--- src/framework/one.clj
+++ src/framework/one.clj
@@ -306,7 +306,7 @@
                   :reload-application-on-every-request false
                   :template :enlive ; or :selmer
                   :suffix "html" ; views / layouts would be .html
-                  :version "0.2.0-SNAPSHOT"}
+                  :version "0.2.1"}
         my-config (framework-defaults (merge defaults (apply hash-map app-config)))]
     (when (= :selmer (:template my-config))
       (selmer.filters/add-filter! :empty? empty?))
