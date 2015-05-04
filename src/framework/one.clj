a791f84d71dda9b83c8426436ea2f49ecdea2d45
Sean Corfield <sean@worldsingles.com>
2015-05-03 23:37:42 -0700

    Cleanup to pass config through framework instead of using a global atom

--- src/framework/one.clj
+++ src/framework/one.clj
@@ -26,10 +26,6 @@
             [selmer.parser]
             [selmer.filters]))
 
-;; application configuration - set in start function
-
-(def ^:private config (atom {}))
-
 ;; bridge in a couple of very useful Selmer symbols
 
 (intern *ns* (with-meta 'add-tag!
@@ -63,7 +59,7 @@
   (assoc rc ::redirect {:status 302 :headers {"Location" url}}))
 
 (defn reload? [rc]
-  (let [config @config
+  (let [config (:config rc)
         reload (get rc (:reload config))
         password (:password config)]
     (or (and reload password (= reload password))
@@ -149,43 +145,40 @@
            route
            (concat route [""]))))
 
-(defn- stem [sep]
-  (let [config @config]
-    (if-let [app (:application-key config)]
-      (str app sep)
-      "")))
+(defn- stem [config sep]
+  (if-let [app (:application-key config)]
+    (str app sep)
+    ""))
 
-(defn- get-view-nodes [section item]
-  (let [config @config]
-    (str (stem "/") "views/" section "/" item "." (:suffix config))))
+(defn- get-view-nodes [config section item]
+  (str (stem config "/") "views/" section "/" item "." (:suffix config)))
 
-(defn- apply-controller [controller-ns rc item]
+(defn- apply-controller [config controller-ns rc item]
   (if (or (::redirect rc)
           (::render rc))
     rc
     (if (keyword? item)
-      (if-let [f (item @config)] (f rc) rc)
+      (if-let [f (item config)] (f rc) rc)
       (if-let [f (resolve (symbol (str controller-ns "/" item)))] (f rc) rc))))
 
-(defn- get-layout-nodes [section item]
-  (let [config @config
-        dot-html (str "." (:suffix config))]
-    [(str (stem "/") "layouts/" section "/" item dot-html)
-     (str (stem "/") "layouts/" section dot-html)
-     (str (stem "/") "layouts/default" dot-html)]))
+(defn- get-layout-nodes [config section item]
+  (let [dot-html (str "." (:suffix config))]
+    [(str (stem config "/") "layouts/" section "/" item dot-html)
+     (str (stem config "/") "layouts/" section dot-html)
+     (str (stem config "/") "layouts/default" dot-html)]))
 
-(defn- apply-view [rc section item]
-  (when-let [view-nodes (get-view-nodes section item)]
+(defn- apply-view [config rc section item]
+  (when-let [view-nodes (get-view-nodes config section item)]
     (try
-      (selmer.parser/render-file view-nodes rc (:selmer-tags @config))
+      (selmer.parser/render-file view-nodes rc (:selmer-tags config))
       (catch Exception _
         nil))))
 
-(defn- apply-layout [rc nodes layout-nodes]
+(defn- apply-layout [config rc nodes layout-nodes]
   (try
     (selmer.parser/render-file layout-nodes
                                (assoc rc :body [:safe nodes])
-                               (:selmer-tags @config))
+                               (:selmer-tags config))
     (catch Exception _
       nodes)))
 
@@ -194,10 +187,10 @@
    :header {"Content-Type" "text/html; charset=utf-8"}
    :body "Not Found"})
 
-(defn- render-page [rc section item]
-  (if-let [view-render (apply-view rc section item)]
-    (let [layout-cascade (get-layout-nodes section item)
-          final-html (reduce (partial apply-layout rc) view-render layout-cascade)]
+(defn- render-page [config rc section item]
+  (if-let [view-render (apply-view config rc section item)]
+    (let [layout-cascade (get-layout-nodes config section item)
+          final-html (reduce (partial apply-layout config rc) view-render layout-cascade)]
       {:status 200
        :headers {"Content-Type" "text/html; charset=utf-8"}
        :body final-html})
@@ -229,11 +222,10 @@
     (catch Exception _
       nil)))
 
-(defn- get-section-item [route]
-  (let [config @config]
-    (if (empty? route)
-      (:home config)
-      [(first route) (or (second route) (:default-item config))])))
+(defn- get-section-item [config route]
+  (if (empty? route)
+    (:home config)
+    [(first route) (or (second route) (:default-item config))]))
 
 (defn- pack-request [rc req]
   (merge
@@ -251,61 +243,58 @@
           resp
           [:session :cookies :flash]))
 
-(defn- render-request [req]
-  (let [config @config
-        [routes new-routes] (:routes config)
+(defn- render-request [config req]
+  (let [[routes new-routes] (:routes config)
         route (process-routes routes new-routes (:uri req) (:request-method req))
-        [section item] (get-section-item route)
+        [section item] (get-section-item config route)
         rc (-> (walk/keywordize-keys (merge (as-map (rest (rest route))) (:params req)))
                (pack-request req)
                (event :action  (str section "." item))
                (event :section section)
                (event :item    item)
                (event :config  config))
-        controller-ns (symbol (str (stem ".") "controllers." section))
+        controller-ns (symbol (str (stem config ".") "controllers." section))
         _ (require-controller rc controller-ns)
-        rc (reduce (partial apply-controller controller-ns)
+        rc (reduce (partial apply-controller config controller-ns)
                    rc
                    [:before "before" item "after" :after])]
     (->> (if-let [redirect (::redirect rc)]
            redirect
            (if-let [render-expr (::render rc)]
              (render-data-response render-expr)
-             (render-page rc section item)))
+             (render-page config rc section item)))
          (unpack-response rc))))
 
-(defn- controller [req]
-  ;; since favicon.ico is commonly requested but often not present, we special case
-  ;; it and return 404 Not Found rather than look for (and fail to find) that action!
-  (if (= "/favicon.ico" (:uri req))
-    (not-found)
-    (try
-      (render-request req)
-      (catch Exception e
-        (if (::handling-exception req)
-          (do
-            (stacktrace/print-stack-trace e)
-            {:status 500
-             :body (str e)})
-          (controller (-> req
-                          (assoc ::handling-exception true)
-                          (assoc :uri (str "/" (first (:error @config)) "/" (second (:error @config))))
-                          (assoc-in [:params :exception] e))))))))
+(defn- controller [config]
+  (fn configured-controller [req]
+    ;; since favicon.ico is commonly requested but often not present, we special case
+    ;; it and return 404 Not Found rather than look for (and fail to find) that action!
+    (if (= "/favicon.ico" (:uri req))
+      (not-found)
+      (try
+        (render-request config req)
+        (catch Exception e
+          (if (::handling-exception req)
+            (do
+              (stacktrace/print-stack-trace e)
+              {:status 500
+               :body (str e)})
+            (configured-controller (-> req
+                                       (assoc ::handling-exception true)
+                                       (assoc :uri (str "/" (first (:error config)) "/" (second (:error config))))
+                                       (assoc-in [:params :exception] e)))))))))
 
-;; convenient handles to Ring's middleware that we use:
-
-(def wrap-params   ring-p/wrap-params)
-(def wrap-flash    ring-f/wrap-flash)
-(def wrap-resource #(ring-r/wrap-resource % (stem "/")))
-
-(defn- wrap-session [h]
-  (cond (= :memory (:session-store @config)) (ring-s/wrap-session h {:store (memory-store)})
-        (= :cookie (:session-store @config)) (ring-s/wrap-session h {:store (cookie-store)})
-        :else (ring-s/wrap-session h)))
-
-(def ^:private default-middleware
+(defn- default-middleware
   "The default set of Ring middleware we apply in FW/1"
-  [wrap-params wrap-flash wrap-session wrap-resource])
+  [config]
+  [ring-p/wrap-params
+   ring-f/wrap-flash
+   (fn [h]
+    (condp = (:session-store config)
+      :memory (ring-s/wrap-session h {:store (memory-store)})
+      :cookie (ring-s/wrap-session h {:store (cookie-store)})
+      (ring-s/wrap-session h)))
+   (fn [req] (ring-r/wrap-resource req (stem config "/")))])
 
 (comment "Example of routes"
 (let [[routes new-routes] (pre-compile-routes
@@ -315,6 +304,16 @@
   (process-routes routes new-routes "/user/42/sort/email"))
 )
 
+(defn- merge-middleware [config]
+  (fn [middleware]
+    (if middleware
+      (condp = (first middleware)
+        :append (concat (default-middleware config) (rest middleware))
+        :replace (rest middleware)
+        :prepend (concat (rest middleware) (default-middleware config))
+        (concat middleware (default-middleware config)))
+      (default-middleware config))))
+
 (defn- framework-defaults [options]
   (assoc options
          :error (if (:error options)
@@ -323,28 +322,25 @@
          :home  (if (:home options)
                   (clojure.string/split (:home options) #"\.")
                   [(:default-section options) (:default-item options)])
-         :routes (pre-compile-routes (:routes options))
-         :middleware (if-let [middleware (:middleware options)]
-                       (condp = (first middleware)
-                         :append (concat default-middleware (rest middleware))
-                         :replace (rest middleware)
-                         :prepend (concat (rest middleware) default-middleware)
-                         (concat middleware default-middleware))
-                       default-middleware)))
+         :routes (pre-compile-routes (:routes options))))
+
+(def ^:private default-options
+  {:after identity
+   :before identity
+   :default-item "default"
+   :default-section "main"
+   :password "secret"
+   :reload :reload
+   :reload-application-on-every-request false
+   :suffix "html" ; views / layouts would be .html
+   :version "0.4.0"})
 
 (defn start [& app-config]
-  (let [defaults {:after identity
-                  :before identity
-                  :default-item "default"
-                  :default-section "main"
-                  :password "secret"
-                  :reload :reload
-                  :reload-application-on-every-request false
-                  :suffix "html" ; views / layouts would be .html
-                  :version "0.4.0"}
-        my-config (framework-defaults (merge defaults (apply hash-map app-config)))]
+  (let [options (merge default-options (apply hash-map app-config))
+        dynamic-options (framework-defaults options)
+        config (update-in dynamic-options [:middleware]
+                          (merge-middleware dynamic-options))]
     (selmer.filters/add-filter! :empty? empty?)
-    (reset! config my-config)
     (reduce (fn [handler middleware] (middleware handler))
-            controller
-            (:middleware my-config))))
+            (controller config)
+            (:middleware config))))
