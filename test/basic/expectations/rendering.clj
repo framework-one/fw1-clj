022fba1d6d0cd8b8fd6d4d8544ae05faca763852
Sean Corfield <sean@corfield.org>
2016-10-02 23:18:56 -0700

    Catch tests up with external route handling

--- test/basic/expectations/rendering.clj
+++ test/basic/expectations/rendering.clj
@@ -9,57 +9,32 @@
                  200                        status
                  "text/html; charset=utf-8" (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
                  "<h1>Hello!</h1>"          body)
-        ((fw1/start {}) {:uri "/renderer/do_html"}))
-
-(expect (more-of {:keys [status headers body]}
-                 204                        status
-                 "text/html; charset=utf-8" (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
-                 "<h1>Hello!</h1>"          body)
-        ((fw1/start {}) {:uri "/renderer/do_html/status/204"}))
+        (((fw1/configure-router {}) :renderer/do_html) {:uri "/renderer/do_html"}))
 
 (expect (more-of {:keys [status headers body]}
                  200                   status
                  "application/json; charset=utf-8"
                  (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
                  "{\"h1\":\"Hello!\"}" body)
-        ((fw1/start {}) {:uri "/renderer/do_json"}))
+        (((fw1/configure-router {}) :renderer/do_json) {:uri "/renderer/do_json"}))
 
 (expect (more-of {:keys [status headers body]}
                  200                   status
                  "application/json; charset=utf-8"
                  (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
                  "{\"H1\":\"Hello!\"}" body)
-        ((fw1/start {:json-config {:key-fn (comp str/upper-case name)}}) {:uri "/renderer/do_json"}))
-
-(expect (more-of {:keys [status headers body]}
-                 500                   status
-                 "application/json; charset=utf-8"
-                 (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
-                 "{\"h1\":\"Bang!\"}" body)
-        ((fw1/start {}) {:uri "/renderer/do_json/status/500"}))
+        (((fw1/configure-router {:json-config {:key-fn (comp str/upper-case name)}})
+          :renderer/do_json) {:uri "/renderer/do_json"}))
 
 (expect (more-of {:keys [status headers body]}
                  200                         status
                  "text/plain; charset=utf-8" (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
                  "Hello!"                    body)
-        ((fw1/start {}) {:uri "/renderer/do_text"}))
-
-(expect (more-of {:keys [status headers body]}
-                 403                         status
-                 "text/plain; charset=utf-8" (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
-                 "With Status"               body)
-        ((fw1/start {}) {:uri "/renderer/do_text/status/403"}))
+        (((fw1/configure-router {}) :renderer/do_text) {:uri "/renderer/do_text"}))
 
 (expect (more-of {:keys [status headers body]}
                  200                       status
                  "text/xml; charset=utf-8" (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
                  "<?xml version=\"1.0\" encoding=\"UTF-8\"?><h1>Hello!</h1>"
                  body)
-        ((fw1/start {}) {:uri "/renderer/do_xml"}))
-
-(expect (more-of {:keys [status headers body]}
-                 201                       status
-                 "text/xml; charset=utf-8" (some (fn [[h v]] (when (= "Content-Type" h) v)) headers)
-                 "<?xml version=\"1.0\" encoding=\"UTF-8\"?><h1>Hello!</h1>"
-                 body)
-        ((fw1/start {}) {:uri "/renderer/do_xml/status/201"}))
+        (((fw1/configure-router {}) :renderer/do_xml) {:uri "/renderer/do_xml"}))
--- test/basic/expectations/server.clj
+++ test/basic/expectations/server.clj
@@ -8,37 +8,17 @@
 (expect (more-of {:keys [status body]}
                  200         status
                  "Default\n" body)
-        ((fw1/start) {:uri "/"}))
+        (((fw1/configure-router {})) {:uri "/"}))
 
 (expect (more-of {:keys [status body]}
                  200           status
                  "Test Item\n" body)
-        ((fw1/start :home "test.item") {:uri "/"}))
+        (((fw1/configure-router {:home "test.item"})) {:uri "/"}))
 
 (expect (more-of {:keys [status body]}
                  200           status
                  "Test Item\n" body)
-        ((fw1/start {:default-section "test" :default-item "item"}) {:uri "/"}))
-
-(expect (more-of {:keys [status body]}
-                 404         status
-                 "Not Found" body)
-        ((fw1/start {:routes [{"$GET/foo" "/"}
-                              {"/:section/:item" "/"}]}) {:uri "/bar" :request-method :post}))
-
-(expect (more-of {:keys [status body]}
-                 404         status
-                 "Not Found" body)
-        ((fw1/start {:routes [{"$GET/foo" "/"}
-                              {"*" "404:/"}]}) {:uri "/bar" :request-method :post}))
-
-(expect (more-of {:keys [status headers body]}
-                 302    status
-                 "/foo" (some (fn [[h v]] (when (= "Location" h) v)) headers)
-                 nil    body)
-        ((fw1/start {:routes [{"$GET/foo" "/"}
-                              {"/bar" "302:/foo"}
-                              {"*" "404:/"}]}) {:uri "/bar" :request-method :post}))
+        (((fw1/configure-router {:default-section "test" :default-item "item"})) {:uri "/"}))
 
 (defn- header-name?
   [name]
@@ -51,4 +31,4 @@
                  6   (count headers)
                  5   (count (filter (header-name? "Access-Control") headers))
                  4   (count (filter (header-name? "Access-Control-Allow") headers)))
-        ((fw1/start) {:uri "/" :request-method :options}))
+        (((fw1/configure-router {})) {:uri "/" :request-method :options}))
